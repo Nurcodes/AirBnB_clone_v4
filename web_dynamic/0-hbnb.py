f60f86e6b127502e6c07c16ca7ed5ace038b3ab7
Oluwatobi Abass <oluwatobiabass5@gmail.com>
2022-02-12 07:18:46 -0800

    updated task 1

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
 from models import storage
+import uuid;
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
+
+
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -37,7 +40,7 @@ def hbnb():
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places
+                           places=places,
                            cache_id=uuid.uuid4())
 
 
